13961f396e5886dd71f5607736dcb95eb827c13e
Naimur Noyon <naimurrahmannoyon@gmail.com>
2023-05-30 19:18:08 +0600

    30May2023

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -211,8 +213,100 @@ class Page2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.green, child: const Center(child: Text('')));
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Linear Percent Indicators"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: 140.0,
+                lineHeight: 14.0,
+                percent: 0.5,
+                center: Text(
+                  "50.0%",
+                  style: new TextStyle(fontSize: 12.0),
+                ),
+                trailing: Icon(Icons.mood),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                backgroundColor: Colors.grey,
+                progressColor: Colors.blue,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new CircularPercentIndicator(
+                //width: 170.0,
+                animation: true,
+                animationDuration: 1000,
+                //lineHeight: 20.0,
+                //leading: new Text("left content"),
+                //trailing: new Text("right content"),
+                percent: 0.8,
+                center: Text("80.0%"),
+                //linearStrokeCap: LinearStrokeCap.butt,
+                progressColor: Colors.red, radius: 50,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2000,
+                percent: 0.9,
+                center: Text("90.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.greenAccent,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2500,
+                percent: 0.8,
+                center: Text("80.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                children: <Widget>[
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.2,
+                    progressColor: Colors.red,
+                  ),
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.5,
+                    progressColor: Colors.orange,
+                  ),
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.9,
+                    progressColor: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
